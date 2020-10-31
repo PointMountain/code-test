3bf5958e7036392106e7253222b03d4107424fb7
Pikachu <749850396@qq.com>
2020-10-31 19:26:20 +0800

    update

--- app/4000.js
+++ app/4000.js
@@ -1,7 +1,49 @@
 const http = require('http')
 
 let server = http.createServer(function (req, res) {
-  res.end('4000')
+  res.writeHead(200,{"Content-Type":'text/plain','charset':'utf-8','Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'PUT,POST,GET,DELETE,OPTIONS'})
+  res.end(JSON.stringify([
+    {
+      id: 1,
+      name: '张一'
+    },
+    {
+      id: 2,
+      name: '张二'
+    },
+    {
+      id: 3,
+      name: '张三'
+    },
+    {
+      id: 4,
+      name: '张四'
+    },
+    {
+      id: 5,
+      name: '张五'
+    },
+    {
+      id: 6,
+      name: '张六'
+    },
+    {
+      id: 7,
+      name: '张七'
+    },
+    {
+      id: 8,
+      name: '张八'
+    },
+    {
+      id: 9,
+      name: '张九'
+    },
+    {
+      id: 10,
+      name: '张十'
+    }
+  ]))
 })
 
 server.listen(4000, function() {
